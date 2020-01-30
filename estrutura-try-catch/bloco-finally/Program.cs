258317dd30bd5f129cc6a120f701fd0149ff11df
Camila Batista <49158437+camissk@users.noreply.github.com>
2020-01-30 11:28:23 -0300

    Add files via upload

---
+++ estrutura-try-catch/bloco-finally/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace bloco_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\TEMP\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Arquivo não encontrado!");
+            }
+            finally
+            {
+                if(fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
+
