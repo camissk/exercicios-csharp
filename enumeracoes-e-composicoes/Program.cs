9f7c8fdb49341c8fefe5bfe2eabfed8cd149815b
Camila Batista <camissk@hotmail.com>
2020-01-30 08:35:23 -0200

    Criando classes e enums

---
+++ enumeracoes-e-composicoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace enumeracoes_e_composicoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
