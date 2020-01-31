3f5d563e979c03cf7b1c860913f6691fe1233c84
Camila Batista <49158437+camissk@users.noreply.github.com>
2020-01-31 15:42:13 -0300

    Add files via upload

---
+++ exercicio-vetores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace exercicio_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos estudantes desejam alugar um quarto? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i}");
+                Console.Write("Insira seu nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Insira seu email: ");
+                string email = Console.ReadLine();
+                Console.Write("Insira o numero do quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos alugados: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ":" + vect[i]);
+                }
+            }
+        }
+    }
+}
