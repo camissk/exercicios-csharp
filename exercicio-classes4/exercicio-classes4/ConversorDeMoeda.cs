c66b9f415945eb79f3208172fa5bdf22a571670f
Camila Batista <49158437+camissk@users.noreply.github.com>
2020-01-31 11:05:07 -0300

    Add files via upload

---
+++ exercicio-classes4/exercicio-classes4/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿namespace exercicio_classes4
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+        public static double ConversorDolarParaReal(double cotacao, double dolar)
+        {
+            double total = cotacao * dolar;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ exercicio-classes4/exercicio-classes4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_classes4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dolar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares voce vai comprar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double conversor = ConversorDeMoeda.ConversorDolarParaReal(cotacao, dolar);
+            
+            Console.WriteLine("Valor a ser pago em reais: " + conversor.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
